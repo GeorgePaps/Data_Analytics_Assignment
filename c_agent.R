a461a38df04073fbefabe09e60794295cdb74d7f
omoschopoulos <43645439+omoschopoulos@users.noreply.github.com>
2018-09-29 21:57:43 +0200

    Update c_agent.R

--- c_agent.R
+++ c_agent.R
@@ -1,8 +1,4 @@
-library(R6)
-
-# test
-
-
+#it includes lemon check, first encounter -> "cooperate" and tit for tat strategy
 Agent <- R6Class("Agent",
                  
                  public = list(
@@ -39,35 +35,40 @@ Agent <- R6Class("Agent",
                      #if we encounter fake agents, greeting with "Lemon!", we defect
                      if(self$greeting_response == "Lemon!"){
                        self$bid = "defect"
-                     } else {                                   #if the agent we encount is not fake
-                       
-                       #if we encounter an opponent for the first time, we cooperate
-                       #find out if this is the first time we encounter an agent
-                       print(self$book)
-                       print(self$my_id)
-                       print(self$opponent_id)
-                       num_enc1 = nrow( self$book[( self$book$id1 == self$my_id & self$book$id2 == self$opponent_id),] )
-                       num_enc2 = nrow( self$book[( self$book$id2 == self$my_id & self$book$id1 == self$opponent_id),] )
-                       
-                       if( num_enc1 + num_enc2 == 0){
-                         first_enc = TRUE
-                       } else {
-                         first_enc = FALSE
-                       }
-                       
-                       if( first_enc == TRUE){
-                         self$bid = "cooperate"
-                       } else{                                 #if this is not our first encounter
-                         
-                         self$bid <- "defect"
-                         
-                       }
-                       
+                     } else {
+                       self$bid = private$tit_for_tat()
                      }
+                   }
+                 ),
+                 
+                 private = list(		 
+                   last_interact = function() {   #interaction between two agents self$id, self$opponent_id
+                     t = self$book  
+                     rec1 = t[(t$id1 == self$my_id & t$id2 == self$opponent_id), c("round","bid2")]  #rec1 is a vector that contains all the lines that (t$id1 == self$id & t$id2 == self$opponent_id) == TRUE, but has only the "round" and "bid2" columns
+                     rec2 = t[(t$id1 == self$opponent_id & t$id2 == self$my_id), c("round","bid1")]
                      
+                     names(rec1) = c("round","bid")   # change names of variables because otherwise there is problem when you will try to combine the two vectors in one
+                     names(rec2) = c("round","bid")
+                     all_rec = rbind(rec1,rec2)       # this vector contains all the encounters of agents self$id, self$opponent_id
                      
-                     
+                     if (nrow(all_rec) > 0) {         # if the two agents have met in the past
+                       round = all_rec$round          # a vector of all the rounds
+                       n = max(round)                 # the largest number in this vector is the last time the two agents met
+                       last = all_rec[all_rec$round == n,"bid"]
+                     } else{                          # if the agents haven't met yet
+                       last = NULL
+                     }
+                     return(last)
+                   },
+                   
+                   tit_for_tat = function() {
+                     their_last = private$last_interact()
+                     if (is.null(their_last)) {
+                       bid = "cooperate"
+                     } else {
+                       bid = their_last
+                     }
+                     return(bid)
                    }
                  )
 )
-
